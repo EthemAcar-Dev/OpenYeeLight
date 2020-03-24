757823c3da3c3e81a52555e08072aa80e8ab7083
Ethem <ethemacar-dev@yandex.com>
2020-03-24 19:53:18 +0100

    Solution renaming

---
+++ OpenYeeLightUI/DeviceViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YeelightAPI;
+
+namespace OpenYeeLightUI
+{
+    public class DeviceViewModel
+    {
+        public Device Device;
+        public string Name;
+        public string Hostname;
+
+        public override string ToString()
+        { return Name; }
+    }
+}
